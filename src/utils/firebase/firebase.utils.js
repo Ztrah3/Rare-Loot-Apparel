60bf88a50af5f6edcac12ef10bc80e3be861928c
Zane <zane1040@gamil.com>
2023-11-15 19:08:15 -0600

    adding comments

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -59,24 +59,32 @@ export const addCollectionAndDocuments = async (
   objectsToAdd,
   field
 ) => {
+  // Create a reference to the collection in Firestore
   const collectionRef = collection(db, collectionKey);
+  // Create a batch to perform multiple write operations
   const batch = writeBatch(db);
 
+  // For each object to add, create a document reference and add it to the batch
   objectsToAdd.forEach((object) => {
     const docRef = doc(collectionRef, object.title.toLowerCase());
     batch.set(docRef, object);
   });
 
+  // Commit the batch
   await batch.commit();
   console.log('done');
 };
 
 // Exporting a function for getting categories and documents from Firestore
 export const getCategoriesAndDocuments = async () => {
+  // Create a reference to the 'categories' collection in Firestore
   const collectionRef = collection(db, 'categories');
+  // Create a query for the collection
   const q = query(collectionRef);
 
+  // Execute the query and get the documents
   const querySnapshot = await getDocs(q);
+  // Return the data of each document
   return querySnapshot.docs.map((docSnapshot) => docSnapshot.data());
 };
 
@@ -85,17 +93,21 @@ export const createUserDocumentFromAuth = async (
   userAuth,
   additionalInformation = {}
 ) => {
+  // If there's no auth user, return
   if (!userAuth) return;
 
+  // Create a reference to the user's document in Firestore
   const userDocRef = doc(db, 'users', userAuth.uid);
-
+  // Get the user's document
   const userSnapshot = await getDoc(userDocRef);
 
+  // If the user's document doesn't exist, create it
   if (!userSnapshot.exists()) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
 
     try {
+      // Set the user's document with the auth user's information and any additional information
       await setDoc(userDocRef, {
         displayName,
         email,
@@ -103,10 +115,11 @@ export const createUserDocumentFromAuth = async (
         ...additionalInformation,
       });
     } catch (error) {
+      // Log any errors that occur when creating the user
       console.log('error creating the user', error.message);
     }
   }
-
+  // Return the reference to the user's document
   return userDocRef;
 };
 
